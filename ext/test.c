33c43885218861c0b7942bb52521145b4f22920a
fukaoi <goodakira7@gmail.com>
2019-04-27 22:37:24 +0900

    modified

---
+++ ext/test.c
@@ -0,0 +1,6 @@
+int node_main(int argc, char* argv[]);
+int main(int argc, char* argv[] )
+{
+    node_main( argc, argv );
+    return 0;
+}
---
+++ src/nodejs.cr
@@ -0,0 +1,7 @@
+# TODO: Write documentation for `Crystal::Nodejs`
+module Crystal::Nodejs
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
+p Crystal::Nodejs::VERSION
