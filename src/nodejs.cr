e9fa0e8c58c2e191c9b984fe71d0cef8a3204abb
fukaoi <goodakira7@gmail.com>
2019-04-28 19:22:12 +0900

    Modified check status

--- src/nodejs.cr
+++ src/nodejs.cr
@@ -8,10 +8,12 @@ module Nodejs
     #todo: process.wait ? fiber nonblocking
     io = IO::Memory.new
     io_error = IO::Memory.new
-    res = Process.run("bin/libnode", args: {"-e", source}, output: io, error: io_error)
-    unless io_error.empty?
+    status = Process.run("bin/libnode", args: {"-e", source}, output: io, error: io_error)
+    unless status.success?
       raise  SystemException.new("Exec libnode: #{io_error.to_s}")
     end
+    io.close
+    io_error.close
     io.to_s.chomp
   end
 end
