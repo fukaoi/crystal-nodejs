d5e4f53ffa32c366b1d32b20f6e4edc84cd7db5d
fukaoi <goodakira7@gmail.com>
2019-05-15 18:35:37 +0900

    a wait commentout

--- src/nodejs/libnodejs.cr
+++ src/nodejs/libnodejs.cr
@@ -1,16 +1,16 @@
-@[Link(ldflags:
-  "-lstdc++ -L#{__DIR__}/../../libnode #{__DIR__}/../../libnode/libbridge.so #{__DIR__}/../../libnode/libnode.so.64"
-)]
-# toodo: module Node
-lib LibNodeJs
+# @[Link(ldflags:
+  # "-lstdc++ -L#{__DIR__}/../../libnode #{__DIR__}/../../libnode/libbridge.so #{__DIR__}/../../libnode/libnode.so.64"
+# )]
+# # toodo: module Node
+# lib LibNodeJs
 
-  struct Tuple
-    type : LibC::Char*
-    response : LibC::Char*
-  end
+  # struct Tuple
+    # type : LibC::Char*
+    # response : LibC::Char*
+  # end
 
-  fun init : Void
-  fun eval(code : LibC::Char*) : Tuple
-  fun deinit : Void
-  fun callback() : Void
-end
+  # fun init : Void
+  # fun eval(code : LibC::Char*) : Tuple
+  # fun deinit : Void
+  # fun callback() : Void
+# end
