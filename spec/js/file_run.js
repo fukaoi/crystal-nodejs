e70b8951ea224e006ab2054f9614eadc8e80fa0c
fukaoi <goodakira7@gmail.com>
2019-08-11 00:14:16 +0900

    Fix module

--- spec/js/file_run.js
+++ spec/js/file_run.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const output = require('./output.js');
+const {output} = require('./output.js');
 fs.readFile('spec/nodejs_spec.cr', 'utf8', (err, text) => {
     output('exec fs.readFile');
     toCrystal({text: text});
